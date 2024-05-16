16253f8759be839c968fd5b4a2a263c7e2b8a7a0
DongHoon Lee <dhl1924@naver.com>
2024-05-16 16:32:12 +0900

    :recycle:[refactor]: 회원가입 성공 메시지 추가

--- hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
+++ hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
@@ -2,10 +2,12 @@ package org.sopt.hyundai.common.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode {
+    MEMBER_CREATE_SUCCESS(200, "성공");
     ;
     private final int status;
     private final String message;

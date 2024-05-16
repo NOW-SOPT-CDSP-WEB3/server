1de3d6c0488b9f4026d96f9d979f515a9055cddd
DongHoon Lee <dhl1924@naver.com>
2024-05-16 19:21:57 +0900

    :sparkles:[feat]: 중복 이메일일 경우 에러 메시지를 반환하는 로직 구현

--- hyundai/src/main/java/org/sopt/hyundai/member/service/MemberService.java
+++ hyundai/src/main/java/org/sopt/hyundai/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package org.sopt.hyundai.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.hyundai.common.dto.ErrorCode;
+import org.sopt.hyundai.exception.BusinessException;
 import org.sopt.hyundai.member.domain.Member;
 import org.sopt.hyundai.member.repository.MemberRepository;
 import org.sopt.hyundai.member.service.dto.MemberCreateRequest;
@@ -14,6 +16,10 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberCreateResponse createMember(MemberCreateRequest memberCreateRequest) {
+        if (memberRepository.existsByEmail(memberCreateRequest.email())) { // 이메일 중복을 잡아줌
+            throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
+        }
+
         Member member = memberRepository.save(Member.create(memberCreateRequest.email(), memberCreateRequest.password()));
         return new MemberCreateResponse(member.getId(), member.getEmail());
     }

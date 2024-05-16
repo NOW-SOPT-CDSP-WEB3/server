b4162653fd985aed124e549c90b3245a7e47abf0
DongHoon Lee <dhl1924@naver.com>
2024-05-16 19:19:41 +0900

    :recycle:[refactor]: @NotBlnak, @Email 어노테이션을 추가하여 예외를 handler에서 잡아줄 수 있도록 구현

--- hyundai/src/main/java/org/sopt/hyundai/member/service/dto/MemberCreateRequest.java
+++ hyundai/src/main/java/org/sopt/hyundai/member/service/dto/MemberCreateRequest.java
@@ -1,7 +1,14 @@
 package org.sopt.hyundai.member.service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record MemberCreateRequest(
+        @NotBlank
+        @Email // 이메일 형식인 지 잡아줌
         String email,
+
+        @NotBlank
         String password
 ) {
 }

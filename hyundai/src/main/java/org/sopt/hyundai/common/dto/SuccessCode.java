c94532a12c4680b52b3b37f8acece595f2e0e32b
DongHoon Lee <dhl1924@naver.com>
2024-05-16 19:20:41 +0900

    :wrench:[chore]: 불필요한 import문 삭제

--- hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
+++ hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
@@ -2,7 +2,6 @@ package org.sopt.hyundai.common.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor

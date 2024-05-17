6801731e141b24e56172985aa617977315ff26e5
DongHoon Lee <dhl1924@naver.com>
2024-05-18 02:05:11 +0900

    :ambulance:[HOTFIX]: 상수 에러 수정

--- hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
+++ hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
@@ -11,8 +11,7 @@ public enum SuccessCode {
     MEMBER_LOGIN_SUCCESS(200, "로그인 성공"),
     GET_ALL_CARD_LIST_SUCCESS(200, "카드 리스트 조회 성공"),
     BOOKMARK_SUCCESS(200, "북마크 요청이 성공했습니다."),
-    GET_CARD_LIST_SUCCESS(200, "카드 리스트 조회 성공")
-    MEMBER_LOGIN_SUCCESS(200, "로그인 성공")
+    GET_CARD_LIST_SUCCESS(200, "카드 리스트 조회 성공"),
     GET_CULTURE_LIST_SUCCESS(HttpStatus.OK.value(), "문화 리스트 조회에 성공했습니다."),
     GET_EVENT_LIST_SUCCESS(HttpStatus.OK.value(), "이벤트 리스트 조회에 성공했습니다.")
     ;

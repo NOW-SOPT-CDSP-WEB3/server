e8a4921eede3ca871bf9727d064073dbbb006ca8
DongHoon Lee <dhl1924@naver.com>
2024-05-18 02:11:06 +0900

    :ambulance:[HOTFIX]: 테스트 빌드를 위한 설정 추가

--- hyundai/src/test/java/org/sopt/hyundai/HyundaiApplicationTests.java
+++ hyundai/src/test/java/org/sopt/hyundai/HyundaiApplicationTests.java
@@ -3,7 +3,7 @@ package org.sopt.hyundai;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HyundaiApplicationTests.class)
 class HyundaiApplicationTests {
 
     @Test

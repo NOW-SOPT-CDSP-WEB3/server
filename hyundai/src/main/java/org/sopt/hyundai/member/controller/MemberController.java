11bc0ffaebaab7c9d040959d6a30ea2532493014
DongHoon Lee <dhl1924@naver.com>
2024-05-17 18:11:27 +0900

    :sparkles:[feat]: 로그인 요청 POST API 추가

--- hyundai/src/main/java/org/sopt/hyundai/member/controller/MemberController.java
+++ hyundai/src/main/java/org/sopt/hyundai/member/controller/MemberController.java
@@ -7,10 +7,13 @@ import org.sopt.hyundai.common.dto.SuccessCode;
 import org.sopt.hyundai.member.service.MemberService;
 import org.sopt.hyundai.member.service.dto.MemberCreateRequest;
 import org.sopt.hyundai.member.service.dto.MemberCreateResponse;
+import org.sopt.hyundai.member.service.dto.MemberLoginRequest;
+import org.sopt.hyundai.member.service.dto.MemberLoginResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,9 +26,17 @@ public class MemberController {
 
     @PostMapping("/sign-up")
     public ResponseEntity<ApiResponse> createMember(
-        @Valid @RequestBody MemberCreateRequest memberCreateRequest) {
+            @Valid @RequestBody MemberCreateRequest memberCreateRequest) {
         MemberCreateResponse response = memberService.createMember(memberCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.of(SuccessCode.MEMBER_CREATE_SUCCESS, response));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> loginMember(
+            @RequestHeader
+            @Valid @RequestBody MemberLoginRequest memberLoginRequest) {
+        MemberLoginResponse response = memberService.loginMember(memberLoginRequest);
+        return ResponseEntity.ok(ApiResponse.of(SuccessCode.MEMBER_LOGIN_SUCCESS, response));
     }
 }

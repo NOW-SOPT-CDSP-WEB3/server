20d485b62f209b447138d3cce3b40fb29dd2710a
DongHoon Lee <dhl1924@naver.com>
2024-05-17 18:10:08 +0900

    :recycle:[refactor]: 로그인 성공 메시지 추가 및 회원가입 성공 메시지 리팩토링

--- hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
+++ hyundai/src/main/java/org/sopt/hyundai/common/dto/SuccessCode.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode {
-    MEMBER_CREATE_SUCCESS(200, "성공");
+    MEMBER_CREATE_SUCCESS(201, "회원가입 성공"),
+    MEMBER_LOGIN_SUCCESS(200, "로그인 성공")
     ;
     private final int status;
     private final String message;
